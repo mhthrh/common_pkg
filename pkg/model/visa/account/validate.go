3f51f14526d912fb1de5f904325c666c7f36e10d
Mohsen <mhthrh@gmail.com>
2025-10-16 13:23:13 -0400

    add validation struct

--- pkg/model/visa/account/validate.go
+++ pkg/model/visa/account/validate.go
@@ -14,3 +14,76 @@ type ValidateResponse struct {
 	ExpectedPostingDate  string             `json:"expectedPostingDate"`
 	Error                visa.ErrorResponse `json:"errorResponse"`
 }
+
+type ValidateR struct {
+	PayoutMethod      string `json:"payoutMethod"`
+	TransactionDetail struct {
+		InitiatingPartyId             string `json:"initiatingPartyId"`
+		BusinessApplicationId         string `json:"businessApplicationId"`
+		TransactionAmount             string `json:"transactionAmount"`
+		TransactionCurrencyCode       string `json:"transactionCurrencyCode"`
+		EndToEndId                    string `json:"endToEndId"`
+		SenderSourceOfFunds           string `json:"senderSourceOfFunds"`
+		SenderBeneficiaryRelationship string `json:"senderBeneficiaryRelationship"`
+		StatementNarrative            string `json:"statementNarrative"`
+		PayoutSpeed                   string `json:"payoutSpeed"`
+		SettlementCurrencyCode        string `json:"settlementCurrencyCode"`
+	} `json:"transactionDetail"`
+	RecipientDetail struct {
+		Type    string `json:"type"`
+		Address struct {
+			AddressLine1 string `json:"addressLine1"`
+			AddressLine2 string `json:"addressLine2"`
+			City         string `json:"city"`
+			PostalCode   string `json:"postalCode"`
+			State        string `json:"state"`
+			Country      string `json:"country"`
+		} `json:"address"`
+		Bank struct {
+			BankName          string `json:"bankName"`
+			AccountName       string `json:"accountName"`
+			AccountNumber     string `json:"accountNumber"`
+			AccountNumberType string `json:"accountNumberType"`
+			CountryCode       string `json:"countryCode"`
+			BankCode          string `json:"bankCode"`
+			BankCodeType      string `json:"bankCodeType"`
+			CurrencyCode      string `json:"currencyCode"`
+		} `json:"bank"`
+		IdentificationList []struct {
+			IdType         string `json:"idType"`
+			IdNumber       string `json:"idNumber"`
+			IdIssueCountry string `json:"idIssueCountry"`
+		} `json:"identificationList"`
+		FirstName      string `json:"firstName"`
+		LastName       string `json:"lastName"`
+		CountryOfBirth string `json:"countryOfBirth"`
+		CityOfBirth    string `json:"cityOfBirth"`
+		ContactNumber  string `json:"contactNumber"`
+		ContactEmail   string `json:"contactEmail"`
+		DateOfBirth    string `json:"dateOfBirth"`
+	} `json:"recipientDetail"`
+	SenderDetail struct {
+		Type    string `json:"type"`
+		Address struct {
+			AddressLine1 string `json:"addressLine1"`
+			AddressLine2 string `json:"addressLine2"`
+			City         string `json:"city"`
+			PostalCode   string `json:"postalCode"`
+			State        string `json:"state"`
+			Country      string `json:"country"`
+		} `json:"address"`
+		IdentificationList []struct {
+			IdType         string `json:"idType"`
+			IdNumber       string `json:"idNumber"`
+			IdIssueCountry string `json:"idIssueCountry"`
+		} `json:"identificationList"`
+		SenderAccountNumber string `json:"senderAccountNumber"`
+		CountryOfBirth      string `json:"countryOfBirth"`
+		CityOfBirth         string `json:"cityOfBirth"`
+		ContactNumber       string `json:"contactNumber"`
+		ContactNumberType   string `json:"contactNumberType"`
+		ContactEmail        string `json:"contactEmail"`
+		Name                string `json:"name"`
+		DateOfBirth         string `json:"dateOfBirth"`
+	} `json:"senderDetail"`
+}
